4e61c9e8fdc1073a9d0a5173107a532467e6b254
Alfian Dimas <kidaliez@gmail.com>
2019-02-08 10:53:25 +0700

    show payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,83 +2,52 @@
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use DB;
+use Session;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+	public function index(){
+    if(!Session::get('login')){
+      return redirect('admin/login')->with('fail','You must login first!');
+    }else{
+      if(Session::get('level') == 1){
+        $payment = DB::table('payment')
+                        ->where('payment.status','p')
+                        ->join('bill','bill.id','=','payment.id_bill')
+                        ->join('usage','usage.id','=','bill.id_usage')
+                        ->join('customer','customer.id','=','usage.id_customer')
+                        ->select('customer.name','customer.kwh_number','payment.date','payment.id_month','payment.year','payment.admin_cost','payment.total','payment.status','payment.bukti')
+                        ->get();
+        $month = array(
+            1 => 'Januari',
+            2 => 'Februari',
+            3 => 'Maret',
+            4 => 'April',
+            5 => 'Mei',
+            6 => 'Juni',
+            7 => 'Juli',
+            8 => 'Agustus',
+            9 => 'September',
+            10 => 'Oktober',
+            11 => 'November',
+            12 => 'D'
+        );
+        $status = array(
+          'p' => 'Pending'
+        );               
+        $data = array(
+          'capt' => 'Pembayaran',
+          'payment'=> $payment,
+          'month' => $month,
+          'status' => $status
+        );
+        return view('admin.payment', $data);
+      }else{
+        return view('errors/403');
+      }
+    }
+  }
 
 }
---
+++ resources/views/admin/payment.blade.php
@@ -0,0 +1,72 @@
+@extends('admin/template')
+@section('content')
+<div class="row clearfix">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <div class="card">
+      <div class="header">
+        <div class="row clearfix">
+          <div class="col-xs-12 col-sm-6">
+            <h2>Daftar Pembayaran</h2>
+          </div>
+        </div>
+      </div>
+      <div class="body">
+        <div class="row">
+          <div class="col-md-12">
+            <table class="table table-striped table-bordered">
+              <tr>
+                <th>No. KWH</th>
+                <th>Nama Pelanggan</th>
+                <th>Tgl. Pembayaran</th>
+                <th>Bulan Bayars</th>
+                <th>Biaya Admin</th>
+                <th>Total</th>
+                <th>Status</th>
+                <th>Bukti</th>
+                <th>Aksi</th>
+              </tr>
+              @foreach ($payment as $data)
+              <tr>
+                <td>{{ $data->name }}</th>
+                <td>{{ $data->kwh_number }}</th>
+                <td>{{ $data->date }}</th>
+                <td>{{ $month[$data->id_month] }} - {{ $data->year }}</th>
+                <td>{{ $data->admin_cost }}</th>
+                <td>{{ $data->total }}</th>
+                <td>{{ $status[$data->status] }}</th>
+                <td><img src="{{ asset('img/verif/') }}"></th>
+                <td>
+                  <a href="" class="btn btn-success">Lunas</a>
+                  <a href="" class="btn btn-danger">Ditolak</a>
+                </th>
+              </tr>
+              @endforeach
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="edit">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title">Tambah level</h4>
+      </div>
+      <div class="modal-body">
+        <form action="" method="post" enctype="multipart/form-data">
+          Nama level 
+          <input type="text" class="form-control" name="nama_level">
+          <br>
+          <input type="submit" name="simpan" value="Simpan" class="btn btn-success">
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
