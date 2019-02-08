74474877d844ea777845319402977ea11c434fc8
Alfian Dimas <kidaliez@gmail.com>
2019-02-09 06:05:16 +0700

    update readme.md add database

---
+++ laravel-ppob.sql
@@ -0,0 +1,360 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2019 at 12:02 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.5.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel-ppob`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `name`, `id_level`) VALUES
+(1, 'aome', '$2y$10$z9gYE0XceEnUa73YCca/Le1cxAnxo/7n8iwHAkXtW5BdbjGrVVfGu', 'Aome', 1),
+(2, 'dimas', '$2y$10$nUMaLJiQ0XoG1Phj/nviauNryxo5/mZNMoU1z06bcrqN7R9ZNRty.', 'Dimas', 1),
+(7, 'scarlet', '$2y$10$Tky6KanrGwWAkIkF9D/1Xedk2nacXXS1AJOD6eP1YJz54aG.hUz.G', 'Scarlet', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `id_usage` int(11) NOT NULL,
+  `month` int(2) NOT NULL,
+  `year` int(4) NOT NULL,
+  `total_meter` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'n'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id`, `id_usage`, `month`, `year`, `total_meter`, `status`) VALUES
+(1, 1, 1, 2019, 85, 'y'),
+(2, 2, 7, 2021, 69, 'n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cost`
+--
+
+CREATE TABLE `cost` (
+  `id` int(11) NOT NULL,
+  `power` varchar(10) NOT NULL,
+  `cost` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cost`
+--
+
+INSERT INTO `cost` (`id`, `power`, `cost`) VALUES
+(1, '450', 900),
+(2, '900', 1800),
+(4, '1800', 3600);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `username` varchar(8) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `kwh_number` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `id_cost` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `username`, `password`, `name`, `kwh_number`, `address`, `id_cost`) VALUES
+(2, 'dimas', '$2y$10$bEmCZDitlq9I1gwyFsgDC.LYxpAGR16D9mh00w/4ffAZMvJaGgt7K', 'Alfian', '20020708', 'Kepanjen', 2),
+(4, 'scarlet', '$2y$10$BtIo3cY9cIVUUtZHd2ImxOg1iTwyvy9xEfGP2d3lDcQzrUYpfW5zu', 'Scarlet', '20020908', 'Malang', 4),
+(5, 'sugara', '$2y$10$GAhZVoSsxTUTvlGpkNynI.SzGSDwNMOzlg.xPlyt9DhMPzy1Bq9VG', 'Sugara', '20020809', 'Kepanjen', 1),
+(6, 'alfian', '$2y$10$DYaB1EiQFHnc05.dh2Kx3uW3UCejWEUBG.bMwt5yZ0pnewC9RTbh6', 'Alfian', '20020908', 'Kepanjen', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`id`, `name`) VALUES
+(1, 'Manager'),
+(2, 'Teller');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `month`
+--
+
+CREATE TABLE `month` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `month`
+--
+
+INSERT INTO `month` (`id`, `name`) VALUES
+(1, 'Januari'),
+(2, 'Februari'),
+(3, 'Maret'),
+(4, 'April'),
+(5, 'Mei'),
+(6, 'Juni'),
+(7, 'Juli'),
+(8, 'Agustus'),
+(9, 'September'),
+(10, 'Oktober'),
+(11, 'November'),
+(12, 'Desember');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL,
+  `id_bill` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `id_month` int(11) NOT NULL,
+  `year` int(4) NOT NULL,
+  `admin_cost` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'p',
+  `bukti` varchar(250) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `id_bill`, `date`, `id_month`, `year`, `admin_cost`, `total`, `status`, `bukti`, `id_admin`) VALUES
+(1, 1, '2019-02-08', 1, 2019, 10000, 153000, 'y', '1549656625.akame.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usage`
+--
+
+CREATE TABLE `usage` (
+  `id` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `month` int(2) NOT NULL,
+  `year` int(4) NOT NULL,
+  `start_meter` float NOT NULL,
+  `finish_meter` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usage`
+--
+
+INSERT INTO `usage` (`id`, `id_customer`, `month`, `year`, `start_meter`, `finish_meter`) VALUES
+(1, 2, 1, 2019, 5, 80),
+(2, 5, 7, 2021, 2, 67);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_level` (`id_level`);
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_usage` (`id_usage`);
+
+--
+-- Indexes for table `cost`
+--
+ALTER TABLE `cost`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_cost` (`id_cost`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `month`
+--
+ALTER TABLE `month`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_bill` (`id_bill`),
+  ADD KEY `id_month` (`id_month`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `usage`
+--
+ALTER TABLE `usage`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_customer` (`id_customer`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `cost`
+--
+ALTER TABLE `cost`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `month`
+--
+ALTER TABLE `month`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `usage`
+--
+ALTER TABLE `usage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`);
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`id_usage`) REFERENCES `usage` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`id_cost`) REFERENCES `cost` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`id_bill`) REFERENCES `bill` (`id`),
+  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`id_month`) REFERENCES `month` (`id`);
+
+--
+-- Constraints for table `usage`
+--
+ALTER TABLE `usage`
+  ADD CONSTRAINT `usage_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- readme.md
+++ readme.md
@@ -1,25 +1,32 @@
-## Laravel PHP Framework
+#UKK Restoran (Laravel)
 
-[![Build Status](https://travis-ci.org/laravel/framework.svg)](https://travis-ci.org/laravel/framework)
-[![Total Downloads](https://poser.pugx.org/laravel/framework/downloads.svg)](https://packagist.org/packages/laravel/framework)
-[![Latest Stable Version](https://poser.pugx.org/laravel/framework/v/stable.svg)](https://packagist.org/packages/laravel/framework)
-[![Latest Unstable Version](https://poser.pugx.org/laravel/framework/v/unstable.svg)](https://packagist.org/packages/laravel/framework)
-[![License](https://poser.pugx.org/laravel/framework/license.svg)](https://packagist.org/packages/laravel/framework)
+Server Requirement (Laravel 5.0)
+PHP >= 5.4, PHP < 7
+Mcrypt PHP Extension
+OpenSSL PHP Extension
+Mbstring PHP Extension
+Tokenizer PHP Extension
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling. Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as authentication, routing, sessions, queueing, and caching.
+How to use this
 
-Laravel is accessible, yet powerful, providing powerful tools needed for large, robust applications. A superb inversion of control container, expressive migration system, and tightly integrated unit testing support give you the tools you need to build any application with which you are tasked.
+1. Download and install xampp with above requirement
+2. Download composer
+3. Clone this project 
+4. Open your cmd and set path to this project
+5. Import laravel-ppob.sql in phpmyadmin
+6. Copy .env.example to .env and set mysql connection
+7. Run "composer install"
+8. Run "php artisan key:generate"
+9. Run "php artisan serve" 
+10. Open your browser and go localhost:8000
+11. Enjoy
 
-## Official Documentation
+Route
+1. localhost:8000/admin -> Manager
+1. localhost:8000/teller -> Teller
+1. localhost:8000/ -> Customer
 
-Documentation for the framework can be found on the [Laravel website](http://laravel.com/docs).
-
-## Contributing
-
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](http://laravel.com/docs/contributions).
-
-### License
-
-The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
-"# laravel-ppob" 
-ECHO is on.
+Account
+1. Manager : user = aome, password = aome
+2. Teller : user = scarlet, password = scarlet
+3. Customer : user = alfian, password = alfian
